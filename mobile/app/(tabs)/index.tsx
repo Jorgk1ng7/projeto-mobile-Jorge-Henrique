f2f8a42f8a20c45e9b5ad17a6748eb059a3a5dba
Ezerth <paulo.emanuel.ribeiro@escola.pr.gov.br>
2025-04-04 12:05:17 -0300

    primeira pagina feita

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-import {Alert, Button, View} from 'react-native';
+import { Image, StyleSheet, Platform, Pressable } from 'react-native';
+import { Alert, Button, View } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useRouter } from 'expo-router';
 
 export default function HomeScreen() {
+  const router = useRouter();
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -19,30 +21,40 @@ export default function HomeScreen() {
         <ThemedText type="title">Olá, Jorge</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">
-          
-        </ThemedText><Button color= "#2843a1"
-  onPress={() => {
-    console.log('You tapped the button!');
-  }}
-  title="pix"
-/>
+        </ThemedText><ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">
+          <Pressable
+            onPress={() => { router.push('/explore');
+
+              console.log('You tapped the button!');
+            }}
+            style={{
+              backgroundColor: "#841584",
+              padding: 6,
+              borderRadius: 100,
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: 38,
+            }}
+          >
+            <ThemedText style={{ color: 'white', fontWeight: 'bold' }}>Pix</ThemedText>
+          </Pressable></ThemedText>
         <ThemedText>
-         <ThemedText type="defaultSemiBold"></ThemedText> 
-        {' '}
+          <ThemedText type="defaultSemiBold"></ThemedText>
+
           <ThemedText type="defaultSemiBold">
             {Platform.select({
-              
+
             })}
           </ThemedText>{' '}
-        
+
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Transferir</ThemedText>
         <ThemedText>
-          
+
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -56,6 +68,7 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
+
   );
 }
 
@@ -76,8 +89,4 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
-  button: {
-    buttonheight: 10 ,
-    width: 10
-  }
 });
